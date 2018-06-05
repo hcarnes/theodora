4cd62a3c441cc975830b1107db3889412e451d54
Hannah Carnes <hecarnes@gmail.com>
2018-06-05 16:47:16 -0400

    create Submission model with with org_id as foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180605204249) do
+ActiveRecord::Schema.define(version: 20180605204602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20180605204249) do
   create_table "orgs", force: :cascade do |t|
     t.string "subdomain"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "submissions", force: :cascade do |t|
+    t.text "title"
+    t.integer "org_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
