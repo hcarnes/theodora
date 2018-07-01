f4e5d1caaec60f2153968dbf4684dfb0228d553b
Hannah Carnes <hecarnes@gmail.com>
2018-06-30 22:09:33 -0400

    create tenant on org creation

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,6 +1,13 @@
 class Organization < ApplicationRecord
+  after_create :create_tenant
 
   def tenant_name
     "organization_#{self.id}"
+  end
+
+  private
+
+  def create_tenant
+    Apartment::Tenant.create(self.tenant_name)
   end
 end
