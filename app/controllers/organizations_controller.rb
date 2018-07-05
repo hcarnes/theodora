d97d55f2bb2a03b29d47bc6dd74931562996328d
Hannah Carnes <hecarnes@gmail.com>
2018-07-04 23:17:06 -0400

    org can be successfully created

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -69,6 +69,6 @@ class OrganizationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def organization_params
-      params.fetch(:organization, {})
+      params.fetch(:organization, {}).permit(:name)
     end
 end
